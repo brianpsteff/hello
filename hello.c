b24b6dfb94f8d5865fbdd8d9e00fd293fc50b115
BRIAN STEFFES <brian@steffes.io>
2015-01-24 21:08:49 -0500

    changed string to a variable

--- hello.c
+++ hello.c
@@ -4,5 +4,8 @@
 
 int main(int argc, char* argv[])
 {
-	printf("\nHello, World of Git!\n");
+	char* my_message = "This is my first successfully initialized repository from \n
+	the command line that is then remotely pushed to GitHub Servers\n";
+
+	printf("\n%s", my_message);
 }
